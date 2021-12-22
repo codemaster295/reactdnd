0d58c8936fe99f136bd943315ef491e0a0d760a5
meetmoradiya <gaminghawkeye14@gmail.com>
2021-12-22 14:21:38 +0530

    first initial comit

---
+++ src/components/Card.jsx
@@ -0,0 +1,50 @@
+import React, { useRef } from 'react'
+import { useDrag, useDrop } from 'react-dnd'
+import ItemTypes from './ItemTypes'
+import CKEditor from '@ckeditor/ckeditor5-react'
+import InlineEditor from '@ckeditor/ckeditor5-build-inline'
+const style = {
+	border: '1px dashed gray',
+	padding: '0.5rem 1rem',
+	marginBottom: '.5rem',
+	backgroundColor: 'white',
+	cursor: 'move',
+}
+const Card = ({ id, text, index, moveCard }) => {
+	const ref = useRef(null)
+	const [{ isOver }, drop] = useDrop({
+		accept: ItemTypes.CARD,
+		drop(item, monitor) {
+			if (!ref.current) {
+				return
+			}
+			const dragIndex = item.index
+			const hoverIndex = index
+			// Don't replace items with themselves
+			if (dragIndex !== hoverIndex) {
+				moveCard(dragIndex, hoverIndex)
+			}
+		},
+		collect: monitor => ({
+			isOver: monitor.isOver(),
+		}),
+	})
+	const [, drag] = useDrag({
+		item: { type: ItemTypes.CARD, id, index },
+	})
+	drag(drop(ref))
+	return (
+		<>
+			<div style={{ border: `1px solid ${isOver ? "blue" : "transparent"}` }} />
+			<div ref={ref} style={{ ...style }}>
+				<CKEditor
+					editor={InlineEditor}
+					data={text}
+					onChange={console.log}
+				/>
+			</div>
+
+		</>
+	)
+}
+export default Card
